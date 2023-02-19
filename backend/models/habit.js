1d6ce96ac3ac03f62bb3cb5bd364924840fede12
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-02-19 19:47:43 +0200

    adding models

---
+++ backend/models/habit.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const habitSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+});
+
+module.exports = mongoose.model('habit', habitSchema);
