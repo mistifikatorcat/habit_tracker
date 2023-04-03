4f299f754e9292c47f39d1b8d6222c466a3e8208
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-04-04 00:50:50 +0300

    attempt to show cards on frontend

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,19 +1,63 @@
 import React from "react";
 
-function Card(){
+function Card({habits, title, description, keyword, status, date, id}){
 
 
 
-    // const [cardRender, setCardRender] = React.useState({})
+    const [cardRender, setCardRender] = React.useState({})
 
 
-    // React.useEffect(() => {
-    //     setCardRender({
-    //         title: cards.title,
-    //         description: cards.description,
-    //         status: cards.status
-    //     })
-    // },)
+    React.useEffect(() => {
+        setCardRender({
+            title: habits.title,
+            description: habits.description,
+            status: habits.status,
+            keyword: habits.keyword,
+            date: habits.createdAt,
+            id: habits._id
+        })
+    }, [])
+
+    const getDate = () => {
+			const habitDate = new Date(habits.publishedAt);
+
+			const months = [
+				'Jan',
+				'Feb',
+				'Mar',
+				'Apr',
+				'May',
+				'Jun',
+				'Jul',
+				'Aug',
+				'Sep',
+				'Oct',
+				'Nov',
+				'Dec',
+			];
+			const month = months[habitDate.getMonth()];
+			const day = habitDate.getDay();
+			const year = habitDate.getFullYear();
+			const fullDate = month + ' ' + day + ',' + year;
+			return fullDate;
+	};
+
+    return(
+        <li className="grid__habit">
+            <div className="habit">
+                <p className="habit__test">{title}</p>
+                <p className="habit__test">{description}</p>
+                <p className="habit__test">{keyword}</p>
+                <p className="habit__test">{status}</p>
+                <p className="habit__test">{getDate}</p>
+                <p className="habit__test">{id}</p>
+
+            </div>
+        </li>
+    )
+
 }
+
+
 
 export default Card;
