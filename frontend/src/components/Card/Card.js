ec9af248911210bb36148e827dd8fa0642ae78aa
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-04-07 00:59:05 +0300

    minor changes

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -19,7 +19,7 @@ function Card({habit, title, description, keyword, status, date, id}){
     }, [])
 
     const getDate = () => {
-			const habitDate = new Date(habit.publishedAt);
+			const habitDate = new Date(habit.createdAt);
 
 			const months = [
 				'Jan',
@@ -38,19 +38,27 @@ function Card({habit, title, description, keyword, status, date, id}){
 			const month = months[habitDate.getMonth()];
 			const day = habitDate.getDay();
 			const year = habitDate.getFullYear();
-			const fullDate = month + ' ' + day + ',' + year;
+			const fullDate = month + ' ' + day + ', ' + year;
 			return fullDate;
 	};
 
     return(
         <li className="grid__habit">
             <div className="habit">
-                <p className="habit__test">{title}</p>
-                <p className="habit__test">{description}</p>
-                <p className="habit__test">{keyword}</p>
-                <p className="habit__test">{status}</p>
-                <p className="habit__test">{getDate}</p>
-                <p className="habit__test">{id}</p>
+                <div className="habit__container">
+                    <h3 className="habit__title">{title}</h3>
+                    <button className="habit__edit"></button>
+                    <button className="habit__delete"></button>
+                    <p className="habit__test">{getDate()}</p>
+                    <div className="habit__text">
+                        <p className="habit__test">{description}</p>    
+                        <p className="habit__test">{keyword}</p>
+                    </div>
+                    <div className="habit__footer">
+                        <p className="habit__test">{status}</p>
+                    </div>
+                </div>
+               
 
             </div>
         </li>
